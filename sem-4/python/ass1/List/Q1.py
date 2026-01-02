c72b15aace35408bd61ad807fe1c899316855284
sayyadshahid <shahidsayyed0212@gmail.com>
2026-01-02 23:34:42 +0530

    added new assignments of sem4

---
+++ sem-4/python/ass1/List/Q1.py
@@ -0,0 +1,5 @@
+# 1. Create a list of 5 favorite foods and print them.
+
+fruits = ['banana', 'apple', 'mango', 'berry', 'aaa' ]
+
+print(fruits, '===============')
---
+++ sem-4/python/ass1/Set/Q1.py
@@ -0,0 +1,4 @@
+# 1. Create a set of 5 favorite colors and print them.
+sett = {'red', 'back', 'white', 'blue', 'yellow'}
+
+print(sett)
